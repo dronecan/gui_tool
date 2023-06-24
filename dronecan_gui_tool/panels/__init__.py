935828a6635767a4cf7d58315feb1a61f5ab9231
Andrew Tridgell <andrew@tridgell.net>
2023-06-24 11:45:42 +1000

    added RTK display panel

--- dronecan_gui_tool/panels/__init__.py
+++ dronecan_gui_tool/panels/__init__.py
@@ -11,6 +11,7 @@ from ..widgets import show_error
 # TODO: Load all inner modules automatically. This is not really easy because we have to support freezing.
 from . import esc_panel
 from . import actuator_panel
+from . import RTK_panel
 
 
 class PanelDescriptor:
@@ -34,5 +35,6 @@ class PanelDescriptor:
 
 PANELS = sorted([
     PanelDescriptor(esc_panel),
-    PanelDescriptor(actuator_panel)
+    PanelDescriptor(actuator_panel),
+    PanelDescriptor(RTK_panel)
 ], key=lambda x: x.name)
