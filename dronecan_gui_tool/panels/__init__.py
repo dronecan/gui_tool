8f04f1eec935531bfdfa87f70ad0cec850a80db8
Andrew Tridgell <andrew@tridgell.net>
2023-07-08 16:50:30 +1000

    rename to serial forwarding

--- dronecan_gui_tool/panels/__init__.py
+++ dronecan_gui_tool/panels/__init__.py
@@ -12,7 +12,7 @@ from ..widgets import show_error
 from . import esc_panel
 from . import actuator_panel
 from . import RTK_panel
-from . import uCenter_panel
+from . import serial_panel
 
 class PanelDescriptor:
     def __init__(self, module):
@@ -37,5 +37,5 @@ PANELS = sorted([
     PanelDescriptor(esc_panel),
     PanelDescriptor(actuator_panel),
     PanelDescriptor(RTK_panel),
-    PanelDescriptor(uCenter_panel)
+    PanelDescriptor(serial_panel)
 ], key=lambda x: x.name)
